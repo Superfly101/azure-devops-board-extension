dc2be6b79ca8d2d1d7b3e66613cdf282828e9ce2
Superfly101 <Danielukoha101@gmail.com>
2025-07-29 18:17:38 +0100

    get the team from the Azure DevOps api

--- src/Samples/work-hub-group/multi-select-dropdown.tsx
+++ src/Samples/work-hub-group/multi-select-dropdown.tsx
@@ -1,34 +1,78 @@
 import * as React from "react";
+import * as SDK from "azure-devops-extension-sdk";
+import {
+    CommonServiceIds,
+    getClient,
+    IProjectPageService,
+} from "azure-devops-extension-api";
+import { CoreRestClient, WebApiTeam } from "azure-devops-extension-api/Core";
+import { WorkRestClient } from "azure-devops-extension-api/Work";
 import { Dropdown } from "azure-devops-ui/Dropdown";
 import { Observer } from "azure-devops-ui/Observer";
 import { DropdownMultiSelection } from "azure-devops-ui/Utilities/DropdownSelection";
-import { IListBoxItem, ListBoxItemType } from "azure-devops-ui/ListBox";
+import { IListBoxItem } from "azure-devops-ui/ListBox";
 
-export const dropdownItems: Array<IListBoxItem<{}>> = [
-    { id: "header", text: "Header", type: ListBoxItemType.Header },
-    { id: "first", text: "first", groupId: "header" },
-    // { id: "divider", type: ListBoxItemType.Divider },
-    // { id: "header2", text: "Header 2", type: ListBoxItemType.Header },
-    { id: "second", text: "second", groupId: "header" },
-    { id: "third", text: "third", groupId: "header" },
-    { id: "fourth", text: "fourth", groupId: "header" },
-    { id: "sixth", text: "sixth", groupId: "header" },
-    { id: "seventh", text: "seventh", groupId: "header" },
-    { id: "eighth", text: "eighth", groupId: "header" },
-    { id: "ninth", text: "ninth", groupId: "header" },
-    { id: "tenth", text: "tenth", groupId: "header" },
-    { id: "eleventh", text: "eleventh", groupId: "header" },
-    { id: "twelvth", text: "twelvth", groupId: "header" },
-    { id: "thirteenth", text: "thirteenth", groupId: "header" },
-    { id: "fourteenth", text: "fourteenth", groupId: "header" },
-    { id: "fifteenth", text: "fifteenth", groupId: "header" }
-];
+interface IMultiSelectState {
+    teams: IListBoxItem<{}>[];
+    loading: boolean;
+}
 
-
-export default class DropdownMultiSelectExample extends React.Component {
+export default class DropdownMultiSelectExample extends React.Component<{}, IMultiSelectState> {
     private selection = new DropdownMultiSelection();
 
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            teams: [],
+            loading: true
+        };
+    }
+
+    public componentDidMount() {
+        this.loadTeams();
+    }
+
+    private async loadTeams() {
+        await SDK.ready();
+        const projectService = await SDK.getService<IProjectPageService>(
+            CommonServiceIds.ProjectPageService
+        );
+        const project = await projectService.getProject();
+        if (!project) {
+            this.setState({ loading: false });
+            return;
+        }
+
+        const coreClient = getClient(CoreRestClient);
+        const workClient = getClient(WorkRestClient);
+
+        const teams = await coreClient.getTeams(project.id);
+        const teamsWithBoardsPromises = teams.map(async (team: WebApiTeam) => {
+            try {
+                const boards = await workClient.getBoards({
+                    project: project.name,
+                    projectId: project.id,
+                    team: team.name,
+                    teamId: team.id
+                });
+                if (boards && boards.length > 0) {
+                    return { id: team.id, text: team.name };
+                }
+            } catch (error) {
+                console.log(`Could not get boards for team ${team.name}: ${error}`);
+            }
+            return null;
+        });
+
+        const results = await Promise.all(teamsWithBoardsPromises);
+        const teamsWithBoards = results.filter(team => team !== null) as Array<IListBoxItem<{}>>;
+
+        this.setState({ teams: teamsWithBoards, loading: false });
+    }
+
     public render() {
+        const { loading, teams } = this.state;
+
         return (
             <div style={{ margin: "8px" }}>
                 <Observer selection={this.selection}>
@@ -48,9 +92,10 @@ export default class DropdownMultiSelectExample extends React.Component {
                                     }
                                 ]}
                                 className="example-dropdown"
-                                items={dropdownItems}
+                                items={teams}
+                                loading={loading}
                                 selection={this.selection}
-                                placeholder="Select an Option"
+                                placeholder="Select a Team"
                                 showFilterBox={true}
                             />
                         );

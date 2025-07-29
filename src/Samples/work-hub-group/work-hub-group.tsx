f10aede8c29e8e0a7b2eb64ea01ecba87dd37218
Superfly101 <Danielukoha101@gmail.com>
2025-07-29 10:19:32 +0100

    Add input to work-hub

--- src/Samples/work-hub-group/work-hub-group.tsx
+++ src/Samples/work-hub-group/work-hub-group.tsx
@@ -5,98 +5,119 @@ import "./work-hub-group.scss";
 
 import { Header } from "azure-devops-ui/Header";
 import { Page } from "azure-devops-ui/Page";
+import { TextField, TextFieldWidth } from "azure-devops-ui/TextField";
+import { FormItem } from "azure-devops-ui/FormItem";
 
 import { showRootComponent } from "../../Common";
-import { CommonServiceIds, IProjectPageService, getClient } from "azure-devops-extension-api";
+import {
+  CommonServiceIds,
+  IProjectPageService,
+  getClient,
+} from "azure-devops-extension-api";
 import { WorkItemTrackingRestClient } from "azure-devops-extension-api/WorkItemTracking";
+import { ObservableValue } from "azure-devops-ui/Core/Observable";
 
 interface IWorkHubGroup {
-    projectContext: any;
-    assignedWorkItems: any[];
+  projectContext: any;
+  assignedWorkItems: any[];
 }
 
+const projectObservable = new ObservableValue<string | undefined>("");
+
 class WorkHubGroup extends React.Component<{}, IWorkHubGroup> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { projectContext: undefined, assignedWorkItems: [] };
+  }
 
-    constructor(props: {}) {
-        super(props);
-        this.state = { projectContext: undefined, assignedWorkItems: [] };
+  public componentDidMount() {
+    try {
+      console.log("Component did mount, initializing SDK...");
+      SDK.init();
+
+      SDK.ready()
+        .then(() => {
+          console.log("SDK is ready, loading project context...");
+          this.loadProjectContext();
+          this.loadAssignedWorkItems();
+        })
+        .catch((error) => {
+          console.error("SDK ready failed: ", error);
+        });
+    } catch (error) {
+      console.error(
+        "Error during SDK initialization or project context loading: ",
+        error
+      );
     }
+  }
 
-    public componentDidMount() {
-        try {
-            console.log("Component did mount, initializing SDK...");
-            SDK.init();
+  public render(): JSX.Element {
+    return (
+      <Page className="sample-hub flex-grow">
+        <Header title="Custom Work Hub" />
+        <div className="page-content">
+          <FormItem label="Project Name" error={false}>
+            <TextField
+              ariaLabel="Project Name"
+              value={projectObservable}
+              onChange={(e, newValue) => {
+                projectObservable.value = newValue;
+              }}
+              width={TextFieldWidth.auto}
+            />
+          </FormItem>
+          
+          <FormItem label="Source of truth Epic ID" error={true}>
+            
+          </FormItem>
 
-            SDK.ready().then(() => {
-                console.log("SDK is ready, loading project context...");
-                this.loadProjectContext();
-                this.loadAssignedWorkItems();
-            }).catch((error) => {
-                console.error("SDK ready failed: ", error);
-            });
-        } catch (error) {
-            console.error("Error during SDK initialization or project context loading: ", error);
-        }
+        </div>
+      </Page>
+    );
+  }
+
+  private async loadProjectContext(): Promise<void> {
+    try {
+      const client = await SDK.getService<IProjectPageService>(
+        CommonServiceIds.ProjectPageService
+      );
+      const context = await client.getProject();
+
+      this.setState({ projectContext: context });
+
+      SDK.notifyLoadSucceeded();
+    } catch (error) {
+      console.error("Failed to load project context: ", error);
     }
+  }
 
-    public render(): JSX.Element {
-        return (
-            <Page className="sample-hub flex-grow">
-                <Header title="Custom Work Hub" />
-                <div className="page-content">
-                    <div className="webcontext-section">
-                        <h2>Project Context:</h2>
-                        <pre>{JSON.stringify(this.state.projectContext, null, 2)}</pre>
-                    </div>
-                    <div className="webcontext-section">
-                        <h2>Work Items Assigned To Me:</h2>
-                        <pre>{JSON.stringify(this.state.assignedWorkItems, null, 2)}</pre>
-                    </div>
-                </div>
-            </Page>
-        );
-    }
+  private async loadAssignedWorkItems(): Promise<void> {
+    try {
+      const client = getClient(WorkItemTrackingRestClient);
 
-    private async loadProjectContext(): Promise<void> {
-        try {
-            const client = await SDK.getService<IProjectPageService>(CommonServiceIds.ProjectPageService);
-            const context = await client.getProject();
-
-            this.setState({ projectContext: context });
-
-            SDK.notifyLoadSucceeded();
-        } catch (error) {
-            console.error("Failed to load project context: ", error);
-        }
-    }
-
-    private async loadAssignedWorkItems(): Promise<void> {
-        try {
-            const client = getClient(WorkItemTrackingRestClient);
-
-            const wiqlQuery = {
-                query:`
+      const wiqlQuery = {
+        query: `
                     SELECT [System.Id], [System.Title]
                     FROM WorkItems
                     WHERE [System.AssignedTo] = @Me
                     ORDER BY [System.ChangedDate] DESC
-                    `
-            };
+                    `,
+      };
 
-            const queryResult = await client.queryByWiql(wiqlQuery);
-            const workItemIds = queryResult.workItems.map((wi) => wi.id);
+      const queryResult = await client.queryByWiql(wiqlQuery);
+      const workItemIds = queryResult.workItems.map((wi) => wi.id);
 
-            if (workItemIds.length > 0) {
-                const workItems = await client.getWorkItems(workItemIds);
-                this.setState({ assignedWorkItems: workItems });
-            }
-            else {
-                this.setState({ assignedWorkItems: [] });
-            }
-        } catch (error) {
-            console.error("Failed to load assigned work items: ", error);
-        }
+      if (workItemIds.length > 0) {
+        const workItems = await client.getWorkItems(workItemIds);
+        this.setState({ assignedWorkItems: workItems });
+      } else {
+        this.setState({ assignedWorkItems: [] });
+      }
+    } catch (error) {
+      console.error("Failed to load assigned work items: ", error);
     }
+  }
 }
 
 showRootComponent(<WorkHubGroup />);
